a3a7a41328316fcc318cd37464f0d8cc2aba6dcf
ibrahimgh25 <62804952+ibrahimgh25@users.noreply.github.com>
2021-08-21 22:12:36 +0300

    Update __init__.py
    Removed unused parts from the source repository

--- dense/fc_densenet/__init__.py
+++ dense/fc_densenet/__init__.py
@@ -1,4 +1,3 @@
 from .fc_densenet import FCDenseNet
 from .transition_up import TransitionUp, CenterCropConcat
 from .transition_down import TransitionDown
-from .standard_fc_densenets import FCDenseNet103
